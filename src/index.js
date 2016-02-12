53e1fe39ac92c7820e03674b115ff708c077deed
Brent Lintner <brent.lintner@gmail.com>
2016-02-12 15:58:30 -0500

    We need to use @forthright org now.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 let _ = require("lodash")
-let vile = require("@brentlintner/vile")
+let vile = require("@forthright/vile")
 
 const ALL_OUTDATED_GEM_LINES = /\*\s.*/ig
 const OUTDATED_GEM_NAME = /\*\s+([^\s]*)/
